4630b1596d87d0fe3a72fdd06cff3fe9828a09d0
Hojiakbar Isroilov <rustamvich7788@gmail.com>
2023-02-03 19:29:08 +0500

    Lesson 2

---
+++ Lessons/Lesson1/script.js
@@ -0,0 +1,17 @@
+"use script"
+
+// Basic Operators
+
+let num = prompt("Yoshingizni kriting")
+
+let a = 10;
+let b = 10;
+let c = 2;
+let d = 10;
+let e = 10;
+let f = 10;
+
+console.log((a+b)/c*d);
+console.log(num);
+
+console.log((a+b)*d+c+e*f);
